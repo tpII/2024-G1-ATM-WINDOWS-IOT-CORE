21d549df0d3b377fbd931e8eef492cdd5c1991b5
Lisandro Martinez <lisandromartz@gmail.com>
2024-12-08 21:18:02 -0300

    Agregar implementacion de repo: CardRepository

--- ATM.Machine/ATM.Machine/ATM.Application/Interfaces/Repositories/ICardRepository.cs
+++ ATM.Machine/ATM.Machine/ATM.Application/Interfaces/Repositories/ICardRepository.cs
@@ -5,6 +5,6 @@ namespace ATM.Application.Interfaces.Repositories;
 
 public interface ICardRepository
 {
-    Task<Card?> GetByIdAsync(string id);
+    Task<bool> ExistsAsync(string id);
     Task<bool> VerifyPinAsync(string cardId, string pin);
 }
---
+++ ATM.Machine/ATM.Machine/ATM.Infrastructure/Repositories/CardRepository.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Text;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using ATM.Application.Interfaces.Repositories;
+using ATM.Domain.Entities;
+
+namespace ATM.Infrastructure.Repositories;
+
+public class CardRepository : ICardRepository
+{
+    private readonly HttpClient _httpClient;
+
+    public CardRepository(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<bool> ExistsAsync(string id)
+    {
+        var response = await _httpClient.GetAsync($"api/cards/{id}/exists");
+
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> VerifyPinAsync(string cardId, string pin)
+    {
+        var jsonPayload = JsonSerializer.Serialize(new { CardId = cardId, Pin = pin });
+        var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("api/cards/verify-pin", content);
+
+        // Si la respuesta es exitosa, devolvemos true. Si no, false.
+        return response.IsSuccessStatusCode;
+    }
+
+}
+    
