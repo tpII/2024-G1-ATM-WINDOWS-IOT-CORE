6adb0d293da16f9a1538985196261e912e4a9919
Lisandro Martinez <lisandromartz@gmail.com>
2024-12-09 23:44:28 -0300

    Agregar conexion a DB y controladores + rutas...
    ...para la api de modelo de cliente

---
+++ ATM.Panel/backend/controllers/client.controller.js
@@ -0,0 +1,62 @@
+import Client from "../models/client.model.js";
+import mongoose from "mongoose";
+
+export const getCount = async(req, res) => {
+    const c = await Client.countDocuments({});
+    console.log(`Contando clintes: ${c}`);
+    res.json({count: c})
+};
+
+export const getClients = async (req,res) => {
+    try {
+        // Usando aggregate() en lugar de find(), se puede realizar un "join" entre colecciones
+        const clients = await Client.aggregate().lookup({
+            from: 'cards',
+            localField: '_id',
+            foreignField: 'clientId',
+            as: 'cards'
+        });
+        res.status(200).json({ success: true, data: clients });
+    } catch (error) {
+        console.log("Ocurrió un error al obtener los usuarios: ", error.message);
+        res.status(404).json({ success: false, message: 'Ocurrió un error al obtener los usuarios' })
+    }
+};
+
+export const createClient = async (req,res) => {
+    const client = req.body;
+
+    if (!client.fullName || !client.email || !client.phone) {
+        return res.status(400).json({ success: false, message: "Por favor provea todos los campos" })
+    }
+
+    const newClient = new Client(client);
+
+    try {
+        await newClient.save();
+        res.status(201).json({ success: true, data: newClient });
+    } catch (error) {
+        console.log(`Error al crear un cliente: ${error.message}`);
+        res.status(500).json({ success: false, message: "Error del servidor" });
+    }
+};
+
+export const deleteClient = async (req,res) => {
+    const {id} = req.params;
+    
+    if(!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(404).json({ success: false, message: "ID inválido" });
+    }
+
+    const client = Client.findById(id);
+    if(!client) return res.status(400).json({ success: false, message: "ID no encontrado" });
+
+    try {
+        await Client.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Cliente borrado" });
+    } catch (error) {
+        console.log("Error al borrar un cliente: ", error.message);
+        res.status(500).json({ success: false, message: "Error del servidor" })
+    }
+};
--- ATM.Panel/backend/index.js
+++ ATM.Panel/backend/index.js
@@ -1,11 +1,26 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+// import accountRoutes from "./routes/account.route.js"
+// import cardRoutes from "./routes/card.route.js"
+import clientRoutes from "./routes/client.route.js"
+// import transactionRoutes from "./routes/transaction.route.js"
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const IP = `${process.env.IP}` || 'locahost';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-});
+app.use(express.json()); //allows us to accept JSON data in the body
 
-app.listen(5000, () => {
-    console.log("Server started at https://localhost:5000");
+// app.use("/api/accounts", clientRoutes);
+// app.use("/api/cards", cardRoutes);
+app.use("/api/clients", clientRoutes);
+// app.use("/api/transactions", transactionRoutes);
+
+app.listen(PORT, IP, () => {
+    connectDB();
+    console.log("Server started at https://" + IP + ":" + PORT);
 });
