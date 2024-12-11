a09825b32910c8448cb9fcb92359a2205b7f7986
Lisandro Martinez <lisandromartz@gmail.com>
2024-12-11 07:00:46 -0300

    Agregar estilos e interacción en la autenticacion

--- ATM.Machine/ATM.Machine/ATM.UI/wwwroot/js/cardNotification.js
+++ ATM.Machine/ATM.Machine/ATM.UI/wwwroot/js/cardNotification.js
@@ -2,17 +2,29 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/cardNotificationHub").build();
 
+const statusDiv = document.getElementById("status");
+
 connection.on("CardDetected", function (isValid) {
-    // document.getElementById("status").innerText = cardId
-    console.log("Card detected");
-    if(isValid)
-    {
-        window.location.replace("/EnterPin");
-    }
-    else
-    {
-        document.getElementById("main-message").innerText = "Ha ocurrido un error al leer la tarjeta";
-        document.getElementById("side-message").innerText = "Intente acercarla nuevamente";
+    // if(isValid)
+    // {
+    //     window.location.replace("/");
+    // }
+    // else
+    // {
+    //     document.getElementById("main-message").innerText = "Ha ocurrido un error al leer la tarjeta";
+    //     document.getElementById("side-message").innerText = "Intente acercarla nuevamente";
+    // }
+    if (isValid) {
+        // Simula un registro exitoso
+        statusDiv.className = "alert alert-success";
+        statusDiv.textContent = "Tarjeta reconocida. Espere un momento...";
+        setTimeout(() => {
+            window.location.href = '/EnterPin'; // Cambiar a la página principal
+        }, 2000);
+    } else {
+        // Simula un error
+        statusDiv.className = "alert alert-danger";
+        statusDiv.textContent = "Error: Tarjeta no reconocida. Por favor, intente nuevamente.";
     }
 });
 
